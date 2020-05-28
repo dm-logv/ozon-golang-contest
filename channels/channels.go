d89d0e04311327165b9d6b9d261c1684bb2ddac6
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-05-28 23:43:22 +0300

    Move f() calculation to separate goroutins

--- channels/channels.go
+++ channels/channels.go
@@ -1,39 +1,39 @@
 package main
 
-import "sync"
-
 type Calc struct {
-	x1     int
-	x2     int
-	result int
+	x1  *int
+	x2  *int
+	out *chan<- int
+}
+
+func (c *Calc) set(x1 *int, x2 *int) {
+	if x1 != nil {
+		c.x1 = x1
+	}
+	if x2 != nil {
+		c.x2 = x2
+	}
+	if c.x1 != nil && c.x2 != nil {
+		*c.out <- *c.x1 + *c.x2
+	}
 }
 
 func Merge2Channels(f func(int) int, in1 <-chan int,
 	in2 <-chan int, out chan<- int, n int) {
 
 	go func() {
-		var calcs []*Calc
-
 		for i := 0; i < n; i++ {
-			x1, x2 := <-in1, <-in2
-			calcs = append(calcs, &Calc{x1, x2, 0})
-		}
-
-		var wg sync.WaitGroup
-		for _, calc := range calcs {
-			wg.Add(1)
-
-			go func(calc *Calc) {
-				defer wg.Done()
-
-				calc.result = f(calc.x1) + f(calc.x2)
-			}(calc)
-		}
-
-		wg.Wait()
-
-		for _, calc := range calcs {
-			out <- calc.result
+			c := Calc{nil, nil, &out}
+			x1 := <-in1
+			x2 := <-in2
+			go func(c *Calc, x1 int) {
+				fx1 := f(x1)
+				c.set(&fx1, nil)
+			}(&c, x1)
+			go func(c *Calc, x2 int) {
+				fx2 := f(x2)
+				c.set(nil, &fx2)
+			}(&c, x2)
 		}
 	}()
 }

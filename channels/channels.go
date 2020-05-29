c46f07993c525293256bab8e8dc77e4225a9aa9d
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-05-29 13:12:14 +0300

    Well, make an another change :(

--- channels/channels.go
+++ channels/channels.go
@@ -1,39 +1,35 @@
 package main
 
 type Calc struct {
-	x1  *int
-	x2  *int
-	out *chan<- int
+	fx1 *int
+	fx2 *int
 }
 
-func (c *Calc) set(x1 *int, x2 *int) {
-	if x1 != nil {
-		c.x1 = x1
-	}
-	if x2 != nil {
-		c.x2 = x2
-	}
-	if c.x1 != nil && c.x2 != nil {
-		*c.out <- *c.x1 + *c.x2
-	}
+func NewCalc(in1 <-chan int, in2 <-chan int, f func(int) int) *Calc {
+	c := Calc{nil, nil}
+
+	go func(c *Calc, in1 <-chan int) {
+		fx1 := f(<-in1)
+		c.fx1 = &fx1
+	}(&c, in1)
+	go func(c *Calc, in2 <-chan int) {
+		fx2 := f(<-in2)
+		c.fx2 = &fx2
+	}(&c, in2)
+
+	return &c
 }
 
 func Merge2Channels(f func(int) int, in1 <-chan int,
 	in2 <-chan int, out chan<- int, n int) {
 
-	go func() {
+	go func(f func(int) int, in1 <-chan int,
+		in2 <-chan int, out chan<- int, n int) {
+
+		calcs := make([]*Calc, n)
+
 		for i := 0; i < n; i++ {
-			c := Calc{nil, nil, &out}
-			x1 := <-in1
-			x2 := <-in2
-			go func(c *Calc, x1 int) {
-				fx1 := f(x1)
-				c.set(&fx1, nil)
-			}(&c, x1)
-			go func(c *Calc, x2 int) {
-				fx2 := f(x2)
-				c.set(nil, &fx2)
-			}(&c, x2)
+			calcs[i] = NewCalc(in1, in2, f)
 		}
-	}()
+	}(f, in1, in2, out, n)
 }

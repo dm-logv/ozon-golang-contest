7a58b71df5c5d8ff63a03d6469431252011951f2
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-05-29 19:55:48 +0300

    Simplest solution with WA :)

--- channels/channels.go
+++ channels/channels.go
@@ -6,5 +6,8 @@ func Merge2Channels(f func(int) int, in1 <-chan int,
 	go func(f func(int) int, in1 <-chan int,
 		in2 <-chan int, out chan<- int, n int) {
 
+		for i := 0; i < n; i++ {
+			out <- f(<-in1) + f(<-in2)
+		}
 	}(f, in1, in2, out, n)
 }

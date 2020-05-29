46391ac30992470926a0267cb8092178f6cf9c16
Dmitry Logvinenko <d.logvinenko@vezet.ru>
2020-05-29 16:31:22 +0300

    Back from the references

--- channels/channels.go
+++ channels/channels.go
@@ -9,14 +9,14 @@ func Merge2Channels(f func(int) int, in1 <-chan int,
 		calcs := make([]*int, n*2)
 		completed := make(chan bool, n*2)
 
-		go read_channel(&calcs, 0, in1, n, f, completed)
-		go read_channel(&calcs, 1, in2, n, f, completed)
+		go read_channel(calcs, 0, in1, n, f, completed)
+		go read_channel(calcs, 1, in2, n, f, completed)
 
-		go printer(&calcs, completed, out, n)
+		go printer(calcs, completed, out, n)
 	}(f, in1, in2, out, n)
 }
 
-func read_channel(calcs *[]*int, shift int, ch <-chan int,
+func read_channel(calcs []*int, shift int, ch <-chan int,
 	n int, f func(int) int, completed chan bool) {
 
 	for i := 0; i < n; i++ {
@@ -24,16 +24,16 @@ func read_channel(calcs *[]*int, shift int, ch <-chan int,
 
 		go func(x int, i int) {
 			fx := f(x)
-			(*calcs)[i+shift] = &fx
+			calcs[i+shift] = &fx
 			completed <- true
 		}(x, i)
 	}
 }
 
-func printer(calcs *[]*int, completed chan bool, out chan<- int, n int) {
+func printer(calcs []*int, completed chan bool, out chan<- int, n int) {
 	printed := 0
 	for <-completed {
-		x1, x2 := (*calcs)[printed], (*calcs)[printed+1]
+		x1, x2 := calcs[printed], calcs[printed+1]
 
 		if x1 != nil && x2 != nil {
 			out <- *x1 + *x2
